946c138fb054fe39138f0bbf506d8223fcde970f
shiroemons <shiroemons@gmail.com>
2017-10-01 12:01:39 +0900

    Add generators setting

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,19 @@ module ExpertTodo
     I18n.enforce_available_locales = true
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :ja
+
+    config.generators do |g|
+      g.orm :active_record
+      g.template_engine :haml
+      g.test_framework :rspec, fixture: true
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+      g.view_specs false
+      g.controller_specs true
+      g.routing_specs false
+      g.helper_specs false
+      g.request_specs false
+      g.assets false
+      g.helper false
+    end
   end
 end
