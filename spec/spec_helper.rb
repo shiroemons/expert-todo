3f75921b8c35185b84d94ffd51f79d5f12bd187c
shiroemons <shiroemons@gmail.com>
2017-10-01 11:58:11 +0900

    Add FactoryGirl setting

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,7 +13,18 @@
 # it.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+require 'factory_girl_rails'
+
 RSpec.configure do |config|
+  config.before :all do
+    FactoryGirl.reload
+    FactoryGirl.factories.clear
+    FactoryGirl.sequences.clear
+    FactoryGirl.find_definitions
+  end
+
+  config.include FactoryGirl::Syntax::Methods
+
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
