5c11eb831eed38d158ddfde8fe7e9e2bb1f1a2ff
shiroemons <shiroemons@gmail.com>
2017-10-09 14:57:16 +0900

    Add user model spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,9 @@ class User < ApplicationRecord
   authenticates_with_sorcery!
 
   validates :email, presence: true
-  validates :password, length: { minimum: 3 }
+  validates :email, uniqueness: true, allow_blank: true
+  validates :password, presence: true
+  validates :password, length: { in: 4..72 }, allow_blank: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true
   validates :password_confirmation, presence: true
-
-  validates :email, uniqueness: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    subject { build(:user) }
+
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_uniqueness_of(:email) }
+    it { is_expected.to validate_presence_of(:password) }
+    it { is_expected.to validate_length_of(:password).is_at_least(4).is_at_most(72) }
+  end
 end
