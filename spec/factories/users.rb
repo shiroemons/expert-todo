98aada9d89dcbafab157b641977a4acd804f58c7
shiroemons <shiroemons@gmail.com>
2017-10-09 14:42:37 +0900

    Change double quote to single quote

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :user do
-    email "MyString"
-    crypted_password "MyString"
-    salt "MyString"
+    email 'MyString'
+    crypted_password 'MyString'
+    salt 'MyString'
   end
 end
